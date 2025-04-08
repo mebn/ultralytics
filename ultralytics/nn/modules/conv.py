7fa208bc466de8dd7d3135928159f43b2902d5f7
Marcus Nilszén <nilszen.marcus@gmail.com>
2025-04-08 13:29:45 +0200

    added ECA

--- ultralytics/nn/modules/conv.py
+++ ultralytics/nn/modules/conv.py
@@ -660,7 +660,19 @@ class SE(nn.Module):
 
 
 class ECA(nn.Module):
-    pass
+    def __init__(self, k_size=3):
+        super().__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.conv = nn.Conv1d(1, 1, kernel_size=k_size, padding=(k_size - 1) // 2, bias=False)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        y = self.avg_pool(x)
+        y = y.squeeze(-1).transpose(-1, -2)
+        y = self.conv(y)
+        y = self.sigmoid(y).transpose(-1, -2).unsqueeze(-1)
+        return x * y.expand_as(x)
+
 
 
 class CA(nn.Module):
